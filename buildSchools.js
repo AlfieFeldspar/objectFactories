1981896944f6d31a879be4c9b865edfea53592d4
Aissa Feldmann <aissaf@umich.edu>
2020-07-12 21:26:48 -0400

    Update buildSchools.js

--- buildSchools.js
+++ buildSchools.js
@@ -61,7 +61,7 @@ class High extends School {
 const lorraineHansbury = new Primary("Lorraine Hansbury", 514, 'Students must be picked up by a parent, guardian, or a family member over the age of 13.');
 lorraineHansbury.quickFacts();
 
-//call static method on the class, not on the instance (call on Primary or School, but not on lorraineHansbury)
+//call static method on the class, not on the instance or the subclass (call on School, but not on lorraineHansbury or Primary)
 const sub = School.pickSubstituteTeacher(['Jamal Crawford', 'Lou Williams', 'J. R. Smith', 'James Harden', 'Jason Terry', 'Manu Ginobli']);
 console.log(sub);
 
